d95b8f6768a254bf5ec27ac49a232ba4b63b113f
Mandeep Kaur <107471933+mandeep-webdev@users.noreply.github.com>
2023-02-21 08:53:55 +0000

    Initial commit

---
+++ cyclicSort.js
@@ -0,0 +1,23 @@
+const cyclicSort = (arr) => {
+  debugger
+  let i = 0
+  while(i<arr.length){
+    let correctIndex = arr[i] - 1
+    if(arr[i]!==arr[correctIndex]) {
+      swap(arr,i,correctIndex)
+    }
+    else {
+      i++
+    }
+  }
+  return arr
+}
+const swap = (arr,index1,index2) => {
+  const temp = arr[index1]
+  arr[index1] = arr[index2]
+  arr[index2] = temp
+}
+
+
+const result = cyclicSort([3,5,2,1,4])
+console.log(result)
